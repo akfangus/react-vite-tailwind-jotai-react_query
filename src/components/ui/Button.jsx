7a7e34fd44d72b0f57fe4e1ddc4359f467eb7b9d
akfangus <akfanakfan@naver.com>
2025-02-10 23:37:34 +0900

    feat: button 컴포넌트 onclick prop 명시적 추가

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -3,7 +3,7 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva } from 'class-variance-authority';
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 focus-visible:ring-4 focus-visible:outline-1 aria-invalid:focus-visible:ring-0",
+    "inline-flex w-fit items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 focus-visible:ring-4 focus-visible:outline-1 aria-invalid:focus-visible:ring-0",
     {
         variants: {
             variant: {
@@ -27,9 +27,16 @@ const buttonVariants = cva(
         },
     },
 );
-function Button({ className, variant, size, asChild = false, ...props }) {
+function Button({ className, onClick, variant, size, asChild = false, ...props }) {
     const Comp = asChild ? Slot : 'button';
 
-    return <Comp data-slot="button" className={cn(buttonVariants({ variant, size, className }))} {...props} />;
+    return (
+        <Comp
+            data-slot="button"
+            className={cn(buttonVariants({ variant, size, className }))}
+            onClick={onClick}
+            {...props}
+        />
+    );
 }
 export { Button, buttonVariants };
